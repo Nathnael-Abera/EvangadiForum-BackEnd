f6735ad00152079ea98330df84c8c8c821dfda20
Nathnael-Abera <NathnaelAbera21@gmail.com>
2023-10-04 11:59:25 +0300

    database deployed

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const port = process.env.PORT||3000||80;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5174',
-    origin: 'https://6eebdb0e.evangadiforum-frontend-72c.pages.dev/'
-}));
-response.setHeader("Access-Control-Allow-Origin", "https://6eebdb0e.evangadiforum-frontend-72c.pages.dev");
+
 
 app.use('/api/users', UserRouter);
 app.use('/api/question', questionRouter);
