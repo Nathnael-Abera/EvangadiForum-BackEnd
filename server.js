6d4bdbeaf4ded15f6d100448853aceaa93f3d766
Nathnael-Abera <NathnaelAbera21@gmail.com>
2023-10-05 20:45:16 +0300

    database redeployed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT||3000||80;
 
-app.use(cors({'http://localhost:5174':true}));
+app.use(cors({origin:true}));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
