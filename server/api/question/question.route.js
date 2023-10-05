7cf3633e8fdc84dfb88e18795c03eaad0bde3564
Nathnael-Abera <NathnaelAbera21@gmail.com>
2023-10-05 20:21:35 +0300

    database redeployed

--- server/api/question/question.route.js
+++ server/api/question/question.route.js
@@ -1,6 +1,13 @@
+const express = require('express')
 const router = require('express').Router();
 const auth = require('../middleware/auth');
 const { createQuestion, getQuestions,getMyQuestion,getById} = require('./question.controller');
+const cors = require('cors');
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 router.post('/', createQuestion);
 router.get('/', getQuestions);
